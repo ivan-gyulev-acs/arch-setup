9f3aff01bcb9d45d51d25b3723e0288ba1d65a72
Ivan Gyulev <i.gyulev23@acsbg.org>
2023-03-11 20:55:48 +0200

    archinstall specific for strix scar II install gdm, gnome and some common apps setup some preferences

---
+++ setup.sh
@@ -0,0 +1,22 @@
+# install yay
+git clone --depth=1 https://aur.archlinux.org/yay.git
+cd yay
+makepkg -i yay
+cd ..
+rm -rf yay
+
+# install some wanted packages
+yay -S $(cat packages.txt)
+
+# change NetworkManager to use iwd
+echo $'[device]\nwifi.backend=iwd' | sudo tee /etc/NetworkManager/conf.d/wifi_backend.conf
+
+# remove grub startup menu
+sudo sed -i 's/GRUB_TIMEOUT=[1-9][1-9]*/GRUB_TIMEOUT=0\nGRUB_HIDDEN_TIMEOUT=0/' /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+# enable important services
+sudo systemctl enable NetworkManager
+sudo systemctl enable iwd
+sudo systemctl enable avahi-daemon
+sudo systemctl enable gdm
